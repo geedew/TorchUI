fd4e5477b352f5325d334e13f2f07efa18d46072
Drew <drew@geedew.com>
2009-08-26 22:22:40 -0500

    Adding TorchUI to GitHub!

---
+++ js/language/lang.en.js
@@ -0,0 +1,10 @@
+
+/* Javascript test file */
+
+ $.torchui.title = "TorchUI Test File v.12";
+ $.torchui.languageLoading = "Language File Loading...";
+ $.torchui.loading = "Loading";
+ $.torchui.pageLoading = "Page currently loading...";
+ 
+ /*Language Testing Variables */
+ $.torchui.languageTitle = "Language Test Passed";
---
+++ js/language/lang.es.js
@@ -0,0 +1,10 @@
+
+ $.torchui.title = "Examino del TorchUI  v.12";
+ $.torchui.languageLoading = "Archivo de idioma de carga...";
+ $.torchui.loading = "Cargando";
+ $.torchui.pageLoading = "Actualmente, la página se está cargando...";
+ 
+ /*Language Testing Variables */
+ $.torchui.languageTitle = "Aprobada la prueba de idioma";
+
+
